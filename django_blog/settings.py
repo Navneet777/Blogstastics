901eb9ebab64fb72314ac85b2978dfe7fba1d798
Navneet777 <navneetchourasiabgt@gmail.com>
2021-05-01 15:34:43 +0530

    add changes

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -51,11 +51,8 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'django_blog.urls'
 
